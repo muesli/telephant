8ab65d8770db7d7dfb125f5fa580cb3313a64845
Christian Muehlhaeuser <muesli@gmail.com>
2019-05-11 02:51:09 +0200

    Simplify LoadConfig code

--- config.go
+++ config.go
@@ -31,7 +31,7 @@ func LoadConfig(configFile string) Config {
 			Theme:    "Material",
 			Style:    "Dark",
 			FirstRun: true,
-			Account:  []Account{Account{}},
+			Account:  []Account{{}},
 		})
 		//log.Fatal("Config file is missing, but a template was created for you! Please edit ", configFile)
 	}
